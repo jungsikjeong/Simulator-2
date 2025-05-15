70e69501a3921b8e1e7f0d6f7bacf302ddc758c8
JungSik Jeong <wndtlr1024@gmail.com>
2025-05-16 08:20:43 +0900

    모바일에서 손가락으로 화면 위아래 못움직이게

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -1,12 +1,28 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { useEffect } from 'react'
 
 export const Route = createFileRoute('/_app')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  useEffect(() => {
+    const handleTouchMove = (e: TouchEvent) => {
+      e.preventDefault()
+    }
+
+    document.addEventListener('touchmove', handleTouchMove, { passive: false })
+
+    return () => {
+      document.removeEventListener('touchmove', handleTouchMove)
+    }
+  }, [])
+
   return (
-    <div className="relative mx-auto h-screen max-w-md overflow-hidden bg-black text-center md:max-w-lg lg:max-w-xl">
+    <div
+      className="relative mx-auto h-screen max-w-md overflow-hidden bg-black text-center md:max-w-lg lg:max-w-xl"
+      onTouchMove={(e) => e.preventDefault()}
+    >
       <main>
         <Outlet />
       </main>
