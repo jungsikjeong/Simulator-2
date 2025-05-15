54e88d32502b91d59e93ef32e0fd5f8cc1511e10
JungSik Jeong <wndtlr1024@gmail.com>
2025-05-16 08:40:59 +0900

    배경색 변경

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,8 +2,8 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <div className="bg-black/80 min-h-screen">
       <Outlet />
-    </>
+    </div>
   ),
 })
