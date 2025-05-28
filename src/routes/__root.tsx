3689d230c18d90894389d260a70a0304b40a0d71
jungsikJeong <wndtlr1024@gmail.com>
2025-05-28 21:03:10 +0900

    흰배경으로 변경

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,9 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="bg-black/80 min-h-screen">
+    <div className="min-h-screen">
       <Outlet />
     </div>
   ),
-})
+});
